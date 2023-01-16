595c8af0730f35d8039805e40e305cd0676e1664
Hoan <hoan@Admins-MacBook-Pro.local>
2023-01-16 08:08:36 +0700

    save

--- src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import myAlert from '~/myAlert'
-
-function Button(props) {
-  return <button onClick={myAlert}>Click me!</button>
-}
-
-export default Button
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout/'
+
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search'
+
+// Public Router
+const publicRouter = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/profile', component: Profile },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+  { path: '/search', component: Search, layout: null },
+]
+
+const privateRouter = []
+
+export { publicRouter, privateRouter }
